927bb88599a54ee9d8b3b657044bd0940db9bcee
Paul Bailey <paul@paulb.me>
2015-06-04 14:50:05 -0500

    1.8.7 fix

--- spec/classes/zookeeper_spec.rb
+++ spec/classes/zookeeper_spec.rb
@@ -22,21 +22,21 @@ describe 'zookeeper' do
           it { is_expected.to contain_file('/opt/zookeeper').with(
             :ensure => 'directory',
             :owner  => 'root',
-            :group  => 'root',
+            :group  => 'root'
           ) }
           it { is_expected.to contain_file('/var/zookeeper').with(
             :ensure => 'directory',
             :owner  => 'root',
-            :group  => 'root',
+            :group  => 'root'
           ) }
           it { is_expected.to contain_file('/var/log/zookeeper').with(
             :ensure => 'directory',
             :owner  => 'root',
-            :group  => 'root',
+            :group  => 'root'
           ) }
           it { is_expected.to contain_staging__deploy('zookeeper-3.4.6.tar.gz').with(
             :target => '/opt/zookeeper',
-            :source => 'http://www.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz',
+            :source => 'http://www.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz'
           ).that_requires(
             'File[/opt/zookeeper]'
           ).that_notifies(
@@ -44,45 +44,45 @@ describe 'zookeeper' do
           ) }
           it { is_expected.to contain_exec('chown install dir').with(
             :command     => '/bin/chown -R root:root /opt/zookeeper/zookeeper-3.4.6',
-            :refreshonly => true,
+            :refreshonly => true
           ) }
           it { is_expected.to contain_file('/opt/zookeeper/current').with(
             :ensure => 'link',
-            :target => '/opt/zookeeper/zookeeper-3.4.6',
+            :target => '/opt/zookeeper/zookeeper-3.4.6'
           ).that_requires('Staging::Deploy[zookeeper-3.4.6.tar.gz]') }
           it { is_expected.to contain_file('/etc/zookeeper').with(
             :ensure => 'link',
-            :target => '/opt/zookeeper/current/conf',
+            :target => '/opt/zookeeper/current/conf'
           ).that_requires('File[/opt/zookeeper/current]') }
 
           it { is_expected.to contain_concat('/opt/zookeeper/zookeeper-3.4.6/conf/zoo.cfg').with(
             :owner => 'root',
             :group => 'root',
-            :mode  => '0644',
+            :mode  => '0644'
           ) }
           it { is_expected.to contain_concat__fragment('header').with(
             :target => '/opt/zookeeper/zookeeper-3.4.6/conf/zoo.cfg',
-            :order  => '01',
+            :order  => '01'
           ) }
           it { is_expected.to contain_concat__fragment('cluster').with(
             :target => '/opt/zookeeper/zookeeper-3.4.6/conf/zoo.cfg',
-            :order  => '05',
+            :order  => '05'
           ) }
           it { is_expected.to contain_file('/var/zookeeper/myid').with(
-            :ensure  => 'file',
+            :ensure  => 'file'
           ).with_content(/1\n/) }
           it { is_expected.to contain_file('/etc/init.d/zookeeper').with(
             :ensure => 'file',
             :owner  => 'root',
             :group  => 'root',
-            :mode   => '0755',
+            :mode   => '0755'
           ).with_content(/ZOOBIN=\"\/opt\/zookeeper\/zookeeper-3.4.6\/bin\"/) }
 
           it { is_expected.to contain_service('zookeeper').with(
             :ensure     => 'running',
             :enable     => true,
             :hasstatus  => true,
-            :hasrestart => true,
+            :hasrestart => true
           ) }
         end
       end
